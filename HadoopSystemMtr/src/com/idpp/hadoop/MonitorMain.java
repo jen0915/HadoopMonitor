ed95a2c96d5a832fde84fe1f823cf5c3e2f9f904
jhlee <ljh9574@gmail.com>
2020-01-05 02:26:50 +0900

    1/5 commit
    Signed-off-by: jhlee <ljh9574@gmail.com>

--- HadoopSystemMtr/src/com/idpp/hadoop/MonitorMain.java
+++ HadoopSystemMtr/src/com/idpp/hadoop/MonitorMain.java
@@ -1,25 +1,45 @@
 package com.idpp.hadoop;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClientBuilder;
-
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Base64;
 
 public class MonitorMain {
 
 	
 	public void action() {
-		String requestURL = "";
+		String requestURL = "http://50.100.100.11:8080/api/v1/clusters/idpp/services/HDFS/components/SECONDARY_NAMENODE";
+		
 		try {
-			HttpClient client = HttpClientBuilder.create().build();
-			HttpGet getRequest = new HttpGet(requestURL); //GET五社球 URL 持失
-			HttpResponse response = client.execute(getRequest);
-			
+	        String name = "admin";
+	        String password = "QKDqwer1234!@";
+
+	        String authString = name + ":" + password;
+	        byte[] authEncBytes = Base64.getEncoder().encode(authString.getBytes());
+	        String authStringEnc = new String(authEncBytes);
+
+	        URL url = new URL(requestURL);
+	        URLConnection urlConnection = url.openConnection();
+	        urlConnection.setRequestProperty("Authorization", "Basic " + authStringEnc);
+	        InputStream is = urlConnection.getInputStream();
+	        InputStreamReader isr = new InputStreamReader(is);
+
+	        int numCharsRead;
+	        char[] charArray = new char[1024];
+	        StringBuffer sb = new StringBuffer();
+	        while ((numCharsRead = isr.read(charArray)) > 0) {
+	            sb.append(charArray, 0, numCharsRead);
+	        }
+	        String result = sb.toString();
+	        System.out.println("result : " + result);
+
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
+
 	public static void main(String[] args) {
 		MonitorMain main = new MonitorMain();
 		main.action();
